c8f3617c5c9abd5e67226d391fa48aee35956be3
Murad Hossain <sliverboymurad@gmail.com>
2020-11-16 22:27:23 +0600

    done all api

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const ObjectId = require('mongodb').ObjectID;
 require('dotenv').config()
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kzt0x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
@@ -28,80 +27,74 @@ const client = new MongoClient(uri, {
 client.connect(err => {
     const bookingCollection = client.db("apartmentHunt").collection("booking");
     const housesCollection = client.db("apartmentHunt").collection("houses");
-    // const reviewsCollection = client.db("apartmentHunt").collection("reviews");
-    const usersCollection = client.db("apartmentHunt").collection("users");
+    // const usersCollection = client.db("apartmentHunt").collection("users");
 
 
     // Add a new User
-    app.post('/addUser', (req, res) => {
-        const admin = req.body;
-        usersCollection.insertOne(admin)
+    // app.post('/addUser', (req, res) => {
+    //     const admin = req.body;
+    //     usersCollection.insertOne(admin)
+    //         .then(result => {
+    //             res.send(result.insertedCount > 0)
+    //         })
+    // });
+
+
+
+    // Booking Request
+    app.post('/bookingRequest', (req, res) => {
+        const name = req.body.name;
+        const customerEmail = req.body.customerEmail;
+        const phone = req.body.phone;
+        const message = req.body.message;
+        const ownerEmail = req.body.ownerEmail;
+        const status = req.body.status;
+        const houseName = req.body.houseName;
+        const price = req.body.price;
+        
+        bookingCollection.insertOne({ name, customerEmail, phone, message, ownerEmail, status, houseName, price })
             .then(result => {
-                res.send(result.insertedCount > 0)
+                res.send(result.insertedCount > 0);
             })
+    });
+
+
+    // Show my house booking list booking by others users
+    app.get('/bookingList/:email', (req, res) => {
+        console.log('bookingLists')
+        bookingCollection
+            .find({ ownerEmail: req.params.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
+    });
+
+
+    // Show my booking list which I booked
+    app.get('/myRent/:email', (req, res) => {
+        bookingCollection
+            .find({ customerEmail: req.params.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
     });
 
     
 
-    // Show all Reviews
-    // app.get('/reviews', (req, res) => {
-    //     reviewsCollection.find({})
-    //         .toArray((err, documents) => {
-    //             res.send(documents);
-    //         })
-    // })
+    // Update Booking status
+    app.patch('/updateStatus/:id', (req, res) => {
+        bookingCollection
+            .updateOne(
+                { _id: ObjectId(req.params.id) },
 
-
-    // Add a new Order
-    // app.post('/addOrder', (req, res) => {
-    //     const name = req.body.name;
-    //     const email = req.body.email;
-    //     const serviceName = req.body.serviceName;
-    //     const description = req.body.description;
-    //     const price = req.body.price;
-    //     const status = req.body.status;
-
-    //     if (req.files) {
-    //         const file = req.files.file;
-    //         const newImg = file.data;
-    //         const encImg = newImg.toString('base64');
-
-    //         var img = {
-    //             contentType: req.files.file.mimetype,
-    //             size: req.files.file.size,
-    //             img: Buffer.from(encImg, 'base64')
-    //         };
-    //     }
-
-    //     ordersCollection.insertOne({ name, email, serviceName, description, price, status, img })
-    //         .then(result => {
-    //             res.send(result.insertedCount > 0);
-    //         })
-    // });
-
-
-    // Show all Orders
-    // app.get('/allOrders', (req, res) => {
-    //     ordersCollection.find({})
-    //         .toArray((err, documents) => {
-    //             res.send(documents);
-    //         })
-    // })
-
-    // Update Order status
-    // app.patch('/updateStatus/:id', (req, res) => {
-    //     ordersCollection
-    //         .updateOne(
-    //             { _id: ObjectId(req.params.id) },
-
-    //             {
-    //                 $set: { status: req.body.status },
-    //             }
-    //         )
-    //         .then((result) => {
-    //             res.send(result.modifiedCount > 0);
-    //         });
-    // });
+                {
+                    $set: { status: req.body.status },
+                }
+            )
+            .then((result) => {
+                res.send(result.modifiedCount > 0);
+            });
+    });
 
 
     // Add new House
@@ -131,15 +124,6 @@ client.connect(err => {
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
-    })
-
-    // Show specifiq user booking
-    app.get('/bookingList/:email', (req, res) => {
-        bookingCollection
-            .find({ email: req.params.email })
-            .toArray((err, documents) => {
-                res.send(documents);
-            });
     });
 
 
